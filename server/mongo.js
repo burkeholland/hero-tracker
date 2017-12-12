e75cc1e581f6f6c066663a478968a836a1e2d2f3
Burke Holland <burkeholland@solcheck.redmond.corp.microsoft.com>
2017-12-12 11:18:40 -0600

    Remove dependency on enviroment file

--- server/mongo.js
+++ server/mongo.js
@@ -1,13 +1,8 @@
 const mongoose = require("mongoose");
-const env = require("./env/environment");
 
 mongoose.Promise = global.Promise;
 
-const mongoUri =
-  process.env.CONNECTION_STRING ||
-  `mongodb://${env.dbName}:${env.key}@${env.dbName}.documents.azure.com:${
-    env.cosmosPort
-  }/?ssl=true`;
+const mongoUri = process.env.CONNECTION_STRING;
 
 function connect() {
   return mongoose.connect(mongoUri, { useMongoClient: true });
