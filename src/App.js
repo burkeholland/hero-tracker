cf6ef41b0901e17d914415597ae2da570701ba52
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2017-12-13 17:40:30 -0500

    Added some Canada to this app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Heroes Updated</h1>
+        <h1>Heroes 🇨🇦</h1>
         <div className="header-bar" />
         <app-heroes />
         <Heroes />
