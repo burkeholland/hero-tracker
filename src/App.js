8e370412105c610138fdfbfeaa900b5b24e96220
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2017-12-15 13:40:39 -0600

    Back home

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Heroes 🇨🇦</h1>
+        <h1>Heroes 🇺🇸</h1>
         <div className="header-bar" />
         <app-heroes />
         <Heroes />
